f5bf6dee7ae7d7c4b3578f6878d0f490655c4892
Ajide Joel <joelajideademola@gmail.com>
2022-04-06 22:50:52 +0100

    deployed election.sol

---
+++ contracts/Election.sol
@@ -0,0 +1,12 @@
+pragma solidity ^0.5.11;
+
+contract Election {
+    // store candidate
+    // read candidate
+    // constructor
+    string public candidate;
+    constructor() public{
+        // initialize candidate
+        candidate = "Candidate 1";
+    }
+}
