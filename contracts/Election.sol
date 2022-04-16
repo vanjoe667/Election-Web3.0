fd3fa2fb209e768b27f3495e88f0a461ed37b5a9
Ajide Joel <joelajideademola@gmail.com>
2022-04-16 12:18:44 +0100

    workin on election

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -1,12 +1,21 @@
 pragma solidity ^0.5.11;
 
 contract Election {
-    // store candidate
-    // read candidate
-    // constructor
-    string public candidate;
+    struct Candidate {
+        uint id;
+        string name;
+        uint voteCount;
+    }
+    mapping(uint => Candidate) public candidates;  // mapping of candidate id to candidate
+    uint public candidatesCount; // number of candidates
     constructor() public{
-        // initialize candidate
-        candidate = "Candidate 1";
+        addCandidate("Candidate 1");
+        addCandidate("Candidate 2");
+        addCandidate("Candidate 3");
+    }
+
+    function addCandidate(string memory _name) private{
+        candidatesCount ++;
+        candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
     }
 }
